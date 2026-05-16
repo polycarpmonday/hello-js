735f3730c774f760060e3bd418bba56133c9037a
Monday Polycarp <polycarpmonday2019@gmail.com>
2026-05-16 05:16:00 -1200

    initial commit: JavaScript Project

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Monday Polycarp"
+const age = 30;
+const favoriteLanguage = "JavaScript";
+
+console.log('Name: ${Monday Polycarp}');
+console.log('Age: ${30}');
+console.log('favorite Language: ${JavaScript}');
